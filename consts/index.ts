f7afbcac6f755c5b0317c3f407010dea8aaab653
bambi-bf <davevasugod@gmail.com>
2024-06-26 16:31:53 +0900

    update config file

--- consts/index.ts
+++ consts/index.ts
@@ -1,7 +1,7 @@
 
 export const COMMITMENT_LEVEL = "confirmed"
-export const RPC = "https://mainnet.helius-rpc.com/?api-key=
-export const WSS = "wss://mainnet.helius-rpc.com/?api-key=
+export const RPC = "https://mainnet.helius-rpc.com/?api-key="
+export const WSS = "wss://mainnet.helius-rpc.com/?api-key="
 export const MINT = "5uwjci3PYeYRnyZiEA3jEpvT3xn4CxjPWocyav1Cu8HT"
 export const MAIN_KP = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 export const DOUBLE_CHECK_INTERVAL=10000
