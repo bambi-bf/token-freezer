5e03c3670b762115c31f1ff82bea41298ce12020
bambi-bf <davevasugod@gmail.com>
2024-06-26 09:31:15 +0200

    add config

---
+++ consts/index.ts
@@ -0,0 +1,8 @@
+
+export const COMMITMENT_LEVEL = "confirmed"
+export const RPC = "https://mainnet.helius-rpc.com/?api-key=
+export const WSS = "wss://mainnet.helius-rpc.com/?api-key=
+export const MINT = "5uwjci3PYeYRnyZiEA3jEpvT3xn4CxjPWocyav1Cu8HT"
+export const MAIN_KP = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+export const DOUBLE_CHECK_INTERVAL=10000
+export const ADDRESS_TO_UNFREEZE = "8adayGtFQeCUWsz55JivCQxy5Q3boVLPJeTAr8HBNhUg"
